279fab6dc3fdcd2dc52eb1b36fcc67827fa17db6
DESKTOP-GVNRQE2\zhasi <zhasik2001@gmail.com>
2021-06-03 13:11:36 +0600

    add exceptions

---
+++ src/main/java/com/mn/wsdlexample/exceptions/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.mn.wsdlexample.exceptions;
+
+public class EntityNotFoundException extends Exception{
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/mn/wsdlexample/exceptions/RequestArgumentException.java
@@ -0,0 +1,8 @@
+package com.mn.wsdlexample.exceptions;
+
+public class RequestArgumentException extends Exception {
+
+    public RequestArgumentException(String message) {
+        super(message);
+    }
+}
